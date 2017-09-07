3e93bdb468a58321def6e645039deda99dd5d501
neal <anilksee@gmail.com>
2017-09-06 22:58:21 -0400

    Custom partition

---
+++ src/main/java/me/anilkc/kafka/partition/SensorProducer.java
@@ -0,0 +1,41 @@
+package me.anilkc.kafka.partition;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * Need to create topic with partitions before running producer. Otherwise, by default it will be
+ * just one partition.
+ * 
+ */
+public class SensorProducer {
+
+  public static void main(String[] args) throws Exception {
+
+    String topicName = "SensorTopic";
+
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("partitioner.class", "me.anilkc.kafka.partition.SensorPartitioner");
+    props.put("speed.sensor.name", "TSS");
+
+    Producer<String, String> producer = new KafkaProducer<>(props);
+
+    for (int i = 0; i < 10; i++) {
+      producer.send(new ProducerRecord<>(topicName, "SSP" + i, "500" + i));
+    }
+
+    for (int i = 0; i < 10; i++) {
+      producer.send(new ProducerRecord<>(topicName, "TSS", "500" + i));
+    }
+
+    producer.close();
+
+    System.out.println("SimpleProducer Completed.");
+  }
+}
